1ea3fd86a38ff8ea7a77ccb3b2f9de5b80ccb05e
jreleaserbot <jreleaser@kordamp.org>
2021-12-09 20:36:46 +0000

    mcs v0.1.7

--- Formula/mcs.rb
+++ Formula/mcs.rb
@@ -1,12 +1,12 @@
 class Mcs < Formula
   desc "Maven Central Search"
   homepage "https://github.com/mthmulders/mcs"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.1.6/mcs-0.1.6-osx-x86_64.zip"
-    sha256 "b5d1bbbb57c7cf175bb60eebd5837acf12dd3d8ec127c4076fbf771500f74112"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.1.7/mcs-0.1.7-osx-x86_64.zip"
+    sha256 "a3a58bcb7931d6aad70d1353e42f570396157d2362604725d00f524e0f84409b"
   end
 
 
@@ -17,6 +17,6 @@ class Mcs < Formula
 
   test do
     output = shell_output("#{bin}/mcs --version")
-    assert_match "0.1.6", output
+    assert_match "0.1.7", output
   end
 end
