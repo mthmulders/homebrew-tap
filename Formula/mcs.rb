3cabb6d5600af26c66fef3bb8dd7fd677e23bd55
jreleaserbot <jreleaser@kordamp.org>
2022-06-20 19:08:08 +0000

    mcs v0.2.1

--- Formula/mcs.rb
+++ Formula/mcs.rb
@@ -1,27 +1,27 @@
-# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z
+# Generated with JReleaser 1.0.0 at 2022-06-20T19:07:57.920017077Z
 class Mcs < Formula
   desc "Maven Central Search"
   homepage "https://github.com/mthmulders/mcs/"
-  version "0.2"
+  version "0.2.1"
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.2/mcs-0.2-linux-x86_64.zip"
-    sha256 "201a5afe15825b68c0a20fc88a9c134162dcf70f3fdca9f02e2f72e027818684"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.2.1/mcs-0.2.1-linux-x86_64.zip"
+    sha256 "04bbc8a1877bc5c136bb0a1c4b754f1d920d1f27d03c791944c0febe5093540a"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.2/mcs-0.2-osx-x86_64.zip"
-    sha256 "ce379a448befc66df1478cec8621aa73be8375e12dcd40825400f6661fda3e7f"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.2.1/mcs-0.2.1-osx-x86_64.zip"
+    sha256 "62c4e196cb7d85382fb41dfb45f576d584671f08d85c2b0eecf9cc01dfa110c0"
   end
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/mcs"
+    bin.install_symlink "#{libexec}/bin/mcs" => "mcs"
   end
 
   test do
     output = shell_output("#{bin}/mcs --version")
-    assert_match "0.2", output
+    assert_match "0.2.1", output
   end
 end
