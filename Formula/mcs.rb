925209064b03a9f88b4bb7252eeeb67d75d02e91
jreleaserbot <jreleaser@kordamp.org>
2025-11-17 09:40:08 +0100

    mcs v0.9.2-rc-10

--- Formula/mcs.rb
+++ Formula/mcs.rb
@@ -1,32 +1,32 @@
-# Generated with JReleaser 1.20.0 at 2025-11-16T15:49:37.641306+01:00
+# Generated with JReleaser 1.20.0 at 2025-11-17T09:39:50.846223+01:00
 
 class Mcs < Formula
   desc "Maven Central Search"
   homepage "https://maarten.mulders.it/projects/mcs/"
-  version "0.9.2-rc-9"
+  version "0.9.2-rc-10"
   license "MIT"
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-9/mcs-0.9.2-rc-9-linux-x86_64.zip"
-    sha256 "a31ae313f7e23397bc80368942072e2a870c7c68507d8e6f21e647da7b8bfc31"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-10/mcs-0.9.2-rc-10-linux-x86_64.zip"
+    sha256 "45475ad4063c9ac378da52e1801f631090cfff9713f06c2d28f4e921f58af90e"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-9/mcs-0.9.2-rc-9-osx-aarch_64.zip"
-    sha256 "db3fd26956992460a37db75bbe498cdd549c25feb62b0dc1139066da5df20295"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-10/mcs-0.9.2-rc-10-osx-aarch_64.zip"
+    sha256 "a473825aa553d315a8485f9e1932bc297adeab56be2409a56eeeb7be7402e501"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-9/mcs-0.9.2-rc-9-osx-x86_64.zip"
-    sha256 "caf331614a29b8a5357df98e6876d2ab2c19b0c26637a77250ae2ca2ed8827ec"
+    url "https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-10/mcs-0.9.2-rc-10-osx-x86_64.zip"
+    sha256 "3f65e550c1b9ede4b0f91319dd7a29393a49eb9cdf91a5e832786775cae57f2b"
   end
 
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/mcs-executable" => "mcs-executable"
+    bin.install_symlink "#{libexec}/bin/mcs" => "mcs"
   end
 
   test do
-    output = shell_output("#{bin}/mcs-executable --version")
-    assert_match "0.9.2-rc-9", output
+    output = shell_output("#{bin}/mcs --version")
+    assert_match "0.9.2-rc-10", output
   end
 end
